658456f92d6c8cbc403603196170a008f8be4853
chunrong <wangcuhnrong@ejiayou.com>
2022-07-11 17:58:39 +0800

    ...

---
+++ composing-builds/src/main/java/ejiayou/composing/builds/EjiayouGradlePlugin.kt
@@ -0,0 +1,14 @@
+package ejiayou.composing.builds
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class EjiayouGradlePlugin : Plugin<Project>{
+    override fun apply(p0: Project) {
+
+    }
+
+    companion object{
+
+    }
+}
